dd94514d2429e98a7e7a1f66bdb478cf9f2945fc
Jedrzej <jedrzej.markowski@sap.com>
2019-10-04 16:55:03 +0200

    update README

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DatePickerModule, TimePickerModule} from '@fundamental-ngx/core';
 import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
